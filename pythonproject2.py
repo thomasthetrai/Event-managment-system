b4ac06d1fcea7132f30899d542ba772e7691a7f3
PythonBeginner <108736120+thomasthetrai@users.noreply.github.com>
2023-07-02 23:45:50 +0200

    Update pythonproject2.py

--- pythonproject2.py
+++ pythonproject2.py
@@ -1,10 +1,10 @@
 Events = [
-    {"Name": "battle of stockholm", "Registrations": 300, "Location": "Vallentuna","ID": "953214949", "Type": "Bjj", "Capacity": [400, 400]},
-    {"Name": "bjj open", "Registrations": 250,"Location": "London","ID": "953214971", "Type": "Bjj", "Capacity": [640, 650]},
-    {"Name": "battle of sandviken", "Registrations": 450, "Location": "Spain","ID": "0903214971", "Type": "Boxing", "Capacity": [250    , 250]},
-    {"Name": "bjj open", "Registrations": 450, "Location": "Ohio","ID": "0904949696", "Type": "muay thai", "Capacity": [100, 250]}
+    {"Name": "battle of stockholm", "Location": "Vallentuna","ID": "953214949", "Type": "Bjj", "Capacity": [400, 400]},
+    {"Name": "bjj open", "Location": "London","ID": "953214971", "Type": "Bjj", "Capacity": [640, 650]},
+    {"Name": "battle of sandviken", "Location": "Spain","ID": "0903214971", "Type": "Boxing", "Capacity": [250, 250]},
+    {"Name": "bjj open", "Location": "Ohio","ID": "0904949696", "Type": "muay thai", "Capacity": [100, 250]}
 ]
-choice = input("Welcome To The Event managment system, what would you like to do today?\n[0] - Quit\n[1] - Register to event\n[2] - Add Event\n[3] - Remove Event(Requires host ID)\n[4] - Check capacity\nEnter: ")
+choice = input("Welcome To The Event managment system, what would you like to do today?\n[0] - Quit\n[1] - Register to event\n[2] - Add Event\n[3] - Remove Event(Requires host ID)\n[4] - Check capacity\n[5] - modify Event(requires login)\nEnter: ")
 def pick_event():
     print(Events[0]["Name"], Events[0]["Location"])
     print(Events[1]["Name"], Events[1]["Location"])
@@ -157,11 +157,149 @@ def Check_capacity():
                     else:
                         if Events[3]["Capacity"][0] < Events[3]["Capacity"][1]:
                             print("Open")
-                
-        
+def Modify_event():
+    mod = input("ID Required\nEnter: ")
+    if mod == Events[0]["ID"]:
+        kok = input("\nWhat would you like to modify for battle of stockholm\n[1] - Name\n[2] - Location\n[3] - Type\nEnter: ")
+        if kok == "1":
+            nmn = input("What Would you like the new name to be?\nEnter: ")
+            new_Event_2 = {
+                "Name": nmn,
+                "Location": Events[0]["Location"],
+                "ID": Events[0]["ID"],
+                "Type": Events[0]["Type"],
+                "Capacity": Events[0]["Capacity"]
+            }
+            print(new_Event_2)
+        else:
+            if kok == "2":
+                nmnn = input("What Would you like the new location to be?\nEnter: ")
+                new_Event_2n = {
+                    "Name": Events[0]["Name"],
+                    "Location": nmnn,
+                    "ID": Events[0]["ID"],
+                    "Type": Events[0]["Type"],
+                    "Capacity": Events[0]["Capacity"]
+                }
+                print(new_Event_2n)
+            else:
+                if kok == "3":
+                    nmnnn = input("What Would you like the new Type to be?\nEnter: ")
+                    new_Event_2nn = {
+                        "Name": Events[0]["Name"],
+                        "Location": Events[0]["Location"],
+                        "ID": Events[0]["ID"],
+                        "Type": nmnnn,
+                        "Capacity": Events[0]["Capacity"]
+                    }
+                    print(new_Event_2nn)
+    else:
+        if mod == Events[1]["ID"]:
+            kok = input("\nWhat would you like to modify for bjj open\n[1] - Name\n[2] - Location\n[3] - Type\nEnter: ")
+            if kok == "1":
+                nmn = input("What Would you like the new name to be?\nEnter: ")
+                new_Event_2 = {
+                    "Name": nmn,
+                    "Location": Events[1]["Location"],
+                    "ID": Events[1]["ID"],
+                    "Type": Events[1]["Type"],
+                    "Capacity": Events[1]["Capacity"]
+                }
+                print(new_Event_2)
+            else:
+                if kok == "2":
+                    nmnn = input("What Would you like the new location to be?\nEnter: ")
+                    new_Event_2n = {
+                        "Name": Events[1]["Name"],
+                        "Location": nmnn,
+                        "ID": Events[1]["ID"],
+                        "Type": Events[1]["Type"],
+                        "Capacity": Events[1]["Capacity"]
+                    }
+                    print(new_Event_2n)
+                else:
+                    if kok == "3":
+                        nmnnn = input("What Would you like the new Type to be?\nEnter: ")
+                        new_Event_2nn = {
+                            "Name": Events[1]["Name"],
+                            "Location": Events[1]["Location"],
+                            "ID": Events[1]["ID"],
+                            "Type": nmnnn,
+                            "Capacity": Events[1]["Capacity"]
+                        }
+                        print(new_Event_2nn)
+        else:
+            if mod == Events[2]["ID"]:
+                kok = input("\nWhat would you like to modify for battle of sandviken\n[1] - Name\n[2] - Location\n[3] - Type\nEnter: ")
+                if kok == "1":
+                    nmn = input("What Would you like the new name to be?\nEnter: ")
+                    new_Event_2 = {
+                        "Name": nmn,
+                        "Location": Events[2]["Location"],
+                        "ID": Events[2]["ID"],
+                        "Type": Events[2]["Type"],
+                        "Capacity": Events[2]["Capacity"]
+                    }
+                    print(new_Event_2)
+                else:
+                    if kok == "2":
+                        nmnn = input("What Would you like the new location to be?\nEnter: ")
+                        new_Event_2n = {
+                            "Name": Events[2]["Name"],
+                            "Location": nmnn,
+                            "ID": Events[2]["ID"],
+                            "Type": Events[2]["Type"],
+                            "Capacity": Events[2]["Capacity"]
+                        }
+                        print(new_Event_2n)
+                    else:
+                        if kok == "3":
+                            nmnnn = input("What Would you like the new Type to be?\nEnter: ")
+                            new_Event_2nn = {
+                                "Name": Events[2]["Name"],
+                                "Location": Events[2]["Location"],
+                                "ID": Events[2]["ID"],
+                                "Type": nmnnn,
+                                "Capacity": Events[2]["Capacity"]
+                            }
+                            print(new_Event_2nn)
+            else:
+                if mod == Events[3]["ID"]:
+                    kok = input("\nWhat would you like to modify for battle of sandviken\n[1] - Name\n[2] - Location\n[3] - Type\nEnter: ")
+                    if kok == "1":
+                        nmn = input("What Would you like the new name to be?\nEnter: ")
+                        new_Event_2 = {
+                            "Name": nmn,
+                            "Location": Events[3]["Location"],
+                            "ID": Events[3]["ID"],
+                            "Type": Events[3]["Type"],
+                            "Capacity": Events[3]["Capacity"]
+                        }
+                        print(new_Event_2)
+                    else:
+                        if kok == "2":
+                            nmnn = input("What Would you like the new location to be?\nEnter: ")
+                            new_Event_2n = {
+                                "Name": Events[3]["Name"],
+                                "Location": nmnn,
+                                "ID": Events[3]["ID"],
+                                "Type": Events[3]["Type"],
+                                "Capacity": Events[3]["Capacity"]
+                            }
+                            print(new_Event_2n)
+                        else:
+                            if kok == "3":
+                                nmnnn = input("What Would you like the new Type to be?\nEnter: ")
+                                new_Event_2nn = {
+                                    "Name": Events[3]["Name"],
+                                    "Location": Events[3]["Location"],
+                                    "ID": Events[3]["ID"],
+                                    "Type": nmnnn,
+                                    "Capacity": Events[3]["Capacity"]
+                                }
+                                print(new_Event_2nn)
 
 
-        
 
 if choice == "0":
     print("")
@@ -177,3 +315,6 @@ else:
             else:
                 if choice == "4":
                     Check_capacity()
+                else:
+                    if choice == "5":
+                        Modify_event()
